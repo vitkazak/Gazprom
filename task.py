0ce03011a68f7b696f42fbffc0fdc716c5587a3b
vkazak <vitaliy.kazak.98@mail.ru>
2022-12-27 18:32:13 +0300

    Test2

--- task.py
+++ task.py
@@ -4,7 +4,7 @@ import requests
 if __name__ == '__main__':
     f = open('log.txt', 'w')
     while True:
-        req = requests.get("https://github.com/vitkazak/Gazpromre")
+        req = requests.get("https://github.com/vitkazak/Gazpromres")
         f.write(str(req) + '\n')
         print(str(req))
 
